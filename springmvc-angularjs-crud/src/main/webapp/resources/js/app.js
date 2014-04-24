3dfdab1dcd1e28607964ed7765fc3317bba8aeda
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2014-04-24 09:27:45 +0530

    Authentication changes.

--- springmvc-angularjs-crud/src/main/webapp/resources/js/app.js
+++ springmvc-angularjs-crud/src/main/webapp/resources/js/app.js
@@ -1,8 +1,12 @@
-var myApp = angular.module('myApp',['ngRoute','users','roles','profiles']);
+var myApp = angular.module('myApp',['ngRoute','users','roles','profiles','login']);
 
-myApp.config(['$routeProvider',
-        function($routeProvider) {
-          $routeProvider.
+myApp.config(['$routeProvider','$locationProvider',
+        function($routeProvider, $locationProvider) {
+          $routeProvider
+          	.when('/login', {
+              templateUrl: 'views/login.html',
+              controller: 'LoginController'
+          	}).
             when('/users', {
               templateUrl: 'views/users.html',
               controller: 'UserController'
@@ -15,8 +19,43 @@ myApp.config(['$routeProvider',
             	templateUrl: 'views/profile.html',
                 controller: 'ProfileController'
             }).
+            when('/main', {
+            	templateUrl: 'views/profile.html',
+                controller: 'MainController'
+            }).
             otherwise({
-              redirectTo: '/users'
+            	redirectTo: 'login'
             });
+          
+          $locationProvider.html5Mode(true); //Remove the '#' from URL.
+}]);
+
+myApp.factory('Auth', function(){
+	var user;
+
+	return {
+	    setUser : function(aUser){
+	    	console.log('setUser:'+aUser);
+	        user = aUser;
+	    },
+	    isLoggedIn : function(){
+	        return(user)? user : false;
+	    }
+	}
+});
+	
+myApp.run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+
+        if (!Auth.isLoggedIn()) {
+            console.log('DENY');
+            event.preventDefault();
+            $location.path('/login');
+        }
+        else {
+            console.log('ALLOW');
+           // $location.path('/users');
+        }
+    });
 }]);
 
